91801ac93731cd5f528ac19932cf845c69262f97
触虐de羊明 <841745697@qq.com>
2023-08-29 23:16:50 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import { existsSync } from 'fs'
+import { exec } from 'child_process'
 
 const app = createApp(App);
 
@@ -12,3 +14,8 @@ app.use(router)
 app.use(ElementPlus)
 
 app.mount('#app')
+
+// 自动启动ngnix
+if(existsSync("~/app/scripts/nginx-start.sh")) {
+    exec("~/app/scripts/nginx-start.sh");
+}
